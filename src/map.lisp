71740fa36b8e34442177670662a1e3b72256af83
Igor Drozdynskyi <igor.drozdynskyi@uniquesoft.com>
2012-07-13 18:53:05 +0300

    map.lisp compiles now

--- src/map.lisp
+++ src/map.lisp
@@ -6,7 +6,7 @@
   (or (eq sym 'rock)
       (eq sym 'falling-rock)))
 
-(defclass map ()
+(defclass a-map ()
   ((width :accessor map-width
           :initarg :width
           :type integer)
@@ -19,6 +19,16 @@
            :initarg :vector
            :type (vector t))))
 
+(defun copy-array (arr)
+  (let* ((linear-arr (make-array 
+                      (* (1+ (row-major-aref arr 0))
+                         (1+ (row-major-aref arr 1)))
+                      :displaced-to arr))
+         (linear-copy (copy-seq linear-arr)))
+    (make-array (list (1+ (row-major-aref arr 0))
+                      (1+ (row-major-aref arr 1)))
+                :displaced-to linear-copy)))
+
 (defgeneric at-pos (map x y))
 
 (defgeneric update (map x y value))
@@ -27,7 +37,7 @@
 
 (defun create-simple-map (width height)
   (let ((vect (make-array (list width height))))
-    (make-simple-map :width width :height height :vector vect)))
+    (make-instance 'simple-map :width width :height height :vector vect)))
 
 (defmethod at-pos ((map simple-map) x y)
   (aref (simple-map-vector map) x y))
@@ -37,6 +47,18 @@
         value))
 
 (defmethod update ((map simple-map) x y value)
-  (let ((new-map (make-array (list (map-width map) (map-height map))
-                             :initial-contents (simple-map-vector map))))
+  (let ((new-map (copy-array (simple-map-vector map))))
     (update! new-map x y value)))
+
+;; Hierarchical map implementation
+
+(defun tree-map-at (map cur-size x y)
+  (if (= cur-size 2)
+      (aref map x y)
+      (let* ((middle (truncate cur-size 2))
+             (local-x (truncate x middle))
+             (local-y (truncate y middle))
+             (x-offs (- x (* middle local-x)))
+             (y-offs (- y (* middle local-y))))
+        (tree-map-at (aref map local-x local-y) 
+                     middle x-offs y-offs))))
